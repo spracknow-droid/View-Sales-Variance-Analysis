6348cb170e6858ae3c7470cb51f01d374ff1b398
spracknow-droid <spracknow@gmail.com>
2026-03-04 14:58:38 +0900

    Update mapping.py

--- mapping.py
+++ mapping.py
@@ -2,11 +2,11 @@
 COLUMNS = {
     'view_name': 'View_Integrated_Sales',
     'date': '매출연월',        # YYYY-MM
-    'cust_group': '고객그룹', # 필터링 축 1
-    'category_mid': '중분류', # 필터링 축 2
-    'division': '데이터구분', # '판매실적' vs '계획' 구분자
+    'cust_group': '고객그룹',  # 필터링 축 1
+    'category_mid': '중분류',  # 필터링 축 2
+    'division': '데이터구분',  # '판매실적' vs '계획' 구분자
     'qty': '수량',
-    'amt_cur': '판매금액',  # (수량×판매단가) 
-    'amt_krw': '장부금액',
+    'amt_cur': '판매금액',    # 외화 기준 (수량 × 판매단가)
+    'amt_krw': '장부금액',    # 원화 기준 (최종 회계 반영액)
     'currency': '거래통화'
 }
