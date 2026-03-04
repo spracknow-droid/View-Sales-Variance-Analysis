215cd901b8e4a5067a1889b6782f4ebb04ef039a
spracknow-droid <spracknow@gmail.com>
2026-03-04 14:51:29 +0900

    Update mapping.py

--- mapping.py
+++ mapping.py
@@ -1,7 +1,7 @@
 # 실제 DB View의 컬럼명과 100% 매칭
 COLUMNS = {
     'view_name': 'View_Integrated_Sales',
-    'date': '매출일',        # 날짜
+    'date': '매출연월',        # YYYY-MM
     'cust_group': '고객그룹', # 필터링 축 1
     'category_mid': '중분류', # 필터링 축 2
     'division': '데이터구분', # '판매실적' vs '계획' 구분자
