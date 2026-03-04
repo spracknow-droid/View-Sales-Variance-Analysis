ded40d7b440e06665001a55a5ea2af93a58667ea
spracknow-droid <spracknow@gmail.com>
2026-03-04 16:39:06 +0900

    Update app.py
    분석 계층 순서 변화

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.sidebar.title("📁 설정")
 uploaded_file = st.sidebar.file_uploader("DB 파일 업로드", type=['db'])
 
 hierarchy_options = {"중분류": COLUMNS['category_mid'], "고객그룹": COLUMNS['cust_group'], "거래통화": COLUMNS['currency']}
-selected_labels = st.sidebar.multiselect("분석 계층 순서", options=list(hierarchy_options.keys()), default=["고객그룹", "중분류", "거래통화"])
+selected_labels = st.sidebar.multiselect("분석 계층 순서", options=list(hierarchy_options.keys()), default=["중분류", "고객그룹", "거래통화"])
 hierarchy = [hierarchy_options[label] for label in selected_labels]
 
 # --- 메인 로직 ---
